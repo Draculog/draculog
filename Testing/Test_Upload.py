1f979bff1dcbe6726e8e6fc427a6a3d94b00e0c9
Daniel Jacoby <djacoby.cs@gmail.com>
2022-07-27 13:03:49 -0700

    Cleaning up Repository

---
+++ Testing/Test_Upload.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+from Draculog import SharedDraculogFunctions
+FrankenWeb = SharedDraculogFunctions()
+
+file = "Results.json"
+
+if os.path.isfile(file):
+    print("I can see the results example file")
+else:
+    print("I can't see the results example file")
+    sys.exit(1)
+
+# import requests as req
+
+# print("Attempting a request call")
+# os.environ['NO_PROXY'] = '127.0.0.1'
+# resp = req.get("https://127.0.0.1:3000/code/notCompiled")
+# print(resp)
+
+if FrankenWeb.Upload_To_FrankenWeb(file):
+    print("Upload did not fail")
+else:
+    print("Upload Failed")
