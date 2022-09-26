6cb3e68624804bb4b624e2146eb96a7e5e4f9cd4
Daniel Jacoby <djacoby.cs@gmail.com>
2022-09-26 12:55:02 -0700

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -4,7 +4,7 @@
 
 apt-get update
 
-#apt-get upgrade
+apt-get upgrade
 
 # Clang install
 apt-get install clang-11 --install-suggests
@@ -19,11 +19,7 @@ apt-get install python3-libgpiod
 
 ## Python3 DHT22 Tools
 #python3 -m pip install --upgrade pip setuptools wheel testresources gpiozero
-
 #pip3 install adafruit-circuitpython-dht adafruit-circuitpython-lis3dh
-
-# Instal for mailing deamon
-apt-get install mailutils
 
 # Python PyRAPL Tools and permissions
 pip install pyRAPL
@@ -38,6 +34,7 @@ chmod -R a+r /sys/class/powercap/intel-rapl
 # Now that everything is installed, build Config file and Params Sample file (possibly build directories to store everything)
 chmod +x Draculog.py
 
+# Draculog instructions to create specific folders and file structures
 python3 Draculog.py bp
 mkdir Source
 python3 Draculog.py bc
