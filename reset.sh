30b01037706683e746513ee4d82c2ba87518af5d
Joshua Gross <gross.joshua.b@gmail.com>
2023-05-06 05:39:37 +0000

    updated reset script

--- reset.sh
+++ reset.sh
@@ -1,6 +1,9 @@
-rm Newly_Executed_Code.txt
-NAME="Source/name_4321"
+NAME="Source/test_238"
+EC_FILE="Newly_Executed_Code.txt"
+DC_FILE="Newly_Downloaded_Code.txt"
+RE_FILE="${NAME}/Results.json"
+[ -f ${EC_FILE} ] && rm $EC_FILE
 [ -d ${NAME} ] || mv ${NAME}* ${NAME}
-echo "${NAME}" > Newly_Downloaded_Code.txt
-rm ${NAME}/Results.json
+echo "${NAME}" > $DC_FILE
+[ -f $RE_FILE ] && rm $RE_FILE
 
